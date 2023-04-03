cece74b47ba98d058cad2bbcd74126d835808baf
Asia <asia_csharpcourse@gmail.com>
2023-04-03 15:55:07 +0200

    eh

---
+++ LibraryManagerTest/Helpers/BooksComparer.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagerTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerTest.Helpers
+{
+    public class BooksComparer
+    {
+        public bool Equals(Book? x, Book? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Id == y.Id && x.Author.Equals(y.Author) && x.Title == y.Title && x.Description == y.Description;
+        }
+
+        public int GetHashCode(Book obj)
+        {
+            return HashCode.Combine(obj.Id, obj.Author, obj.Title, obj.Description);
+        }
+    }
+}
